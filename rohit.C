7e69f84c20fdd71a8e5569c322ae6c0e930058ce
rohitbandgar <bandgarrohit18@gmail.com>
2024-10-05 16:05:54 +0530

    Add files via upload

---
+++ rohit.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+    int no;
+    printf("Enter Number");
+    scanf("%d",&no);
+    printf("%d\n",no*1);
+    printf("%d\n",no*2);
+    printf("%d\n",no*3);
+    printf("%d\n",no*3);
+    printf("%d\n",no*4);
+    printf("%d\n",no*5);
+    printf("%d\n",no*6);
+    printf("%d\n",no*6);
+    printf("%d\n",no*7);
+    printf("%d\n",no*8);
+    printf("%d\n",no*9);
+    printf("%d\n",no*10);
+    return 0;
+}
+
+
+
