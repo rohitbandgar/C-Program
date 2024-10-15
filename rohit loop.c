31121753271a504b001e2e05b690094a463de378
rohitbandgar <bandgarrohit18@gmail.com>
2024-10-15 14:54:58 +0530

    Add files via upload

---
+++ rohit loop.c
@@ -0,0 +1,13 @@
+//loopl.c
+//to print hello world 10 times
+#include <stdio.h>//
+int main()
+{
+    int i =0;
+    while (i<10)
+    {
+        printf("%d\n",i);
+        i++;//update iterator
+    }
+    return 0;
+}    
